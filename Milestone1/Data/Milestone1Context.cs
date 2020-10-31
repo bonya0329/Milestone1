5eaa88276ef5b3665a1bf5554c972f887e72f24e
bonya0329 <60532283+bonya0329@users.noreply.github.com>
2020-10-31 10:38:30 +0600

    Добавьте файлы проекта.

---
+++ Milestone1/Data/Milestone1Context.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Milestone1.Models;
+
+namespace Milestone1.Data
+{
+    public class Milestone1Context : DbContext
+    {
+        public Milestone1Context (DbContextOptions<Milestone1Context> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Milestone1.Models.Product> Product { get; set; }
+    }
+}
